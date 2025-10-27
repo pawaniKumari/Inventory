4f354830a742d9d370ab0296d00c2a7d3577469c
Pawani Kumari <pawanipushpakumari7@gmail.com>
2025-10-27 07:09:14 +0530

    update currency format to display prices in Rs instead of $

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -39,7 +39,7 @@ const CardPopularProducts = () => {
                     </div>
                     <div className="flex text-sm items-center">
                       <span className="font-bold text-blue-500 text-xs">
-                        ${product.price}
+                        Rs{product.price}
                       </span>
                       <span className="mx-2">|</span>
                       <Rating rating={product.rating || 0} />
